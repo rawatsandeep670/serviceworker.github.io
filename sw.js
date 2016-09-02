bc4229f53da6ec26b429d5352332f0b917895943
sandeep <rawatsandeep670@gmail.com>
2016-09-03 00:27:09 +0530

    serviceWorker first commit

--- sw.js
+++ sw.js
@@ -7,5 +7,21 @@ self.addEventListener('activate', function(event){
 
 self.addEventListener('fetch', function(event){
   console.log('fetch : '+event.request.url);
+  let notificationTitle = 'Hello';
+  const notificationOptions = {
+    body: 'Thanks for sending this push msg.',
+    icon: './images/icon-192x192.png',
+    badge: './images/icon-72x72.png',
+    tag: 'simple-push-demo-notification',
+    data: {
+      url: 'https://developers.google.com/web/fundamentals/getting-started/push-notifications/'
+    }
+  };
+  event.waitUntil(
+    Promise.all([
+      self.registration.showNotification(
+        notificationTitle, notificationOptions)
+    ])
+  );
   // return something for each interception
 });
