dca817e92ba0ed90c057199a8e7226c26026c5a8
sandeep <rawatsandeep670@gmail.com>
2016-09-04 16:32:14 +0530

    event send

--- main.js
+++ main.js
@@ -3,6 +3,27 @@
 
         var btn = document.querySelector(".push-btn");
         btn.addEventListener("click", function(event){
+            if ('Notification' in window) {
+            // Check for permission
+                if(Notification.permission=='granted') {
+                    // Get service worker to show notification
+                    self.registration.showNotification(notificationData.data.title, {  
+                        body: notificationData.data.body,
+                        icon: 'favicon.ico' 
+                    }); 
+                }
+                else {
+                //We need to ask permission
+                Notification.requestPermission(function(permission) {
+                    if(permission=='granted') {
+                    self.registration.showNotification(notificationData.data.title, {  
+                        body: notificationData.data.body,
+                        icon: 'favicon.ico' 
+                    });  
+                    }          
+                });
+                }
+            }
              navigator.serviceWorker.controller.postMessage("Service Worker");
         });
         navigator.serviceWorker.addEventListener('message', function(event){
